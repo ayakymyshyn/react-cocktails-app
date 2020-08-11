ce5299df8d1e9d1d49a21dbdafddb25105bbea4f
Yakymyshyn <aiyn@gft.com>
2020-08-11 11:55:19 +0200

    added simple tests for useAutocomplete hook

--- src/components/SearchAutocomplete/hooks/useAutocomplete.ts
+++ src/components/SearchAutocomplete/hooks/useAutocomplete.ts
@@ -1,14 +1,11 @@
 // Core
 import { useState } from 'react';
-// Types
-import { Cocktail } from '../../../types/cocktailsTypes';
 
 export const useAutocomplete = <T>(
   data: Array<T>,
   property?: Partial<T>,
 ) => {
   // Declaring state
-
   const [query, setQuery] = useState('');
   const [results, setResults] = useState<T[]>([]);
 
@@ -30,10 +27,10 @@ export const useAutocomplete = <T>(
 
   // Getting keys
   const getObjectKeys = (
-    criteria: Partial<Cocktail>,
-  ) => Object.keys(criteria) as (keyof Cocktail)[];
+    criteria: Partial<T>,
+  ) => Object.keys(criteria) as (keyof T)[];
 
-  const findResults: Overloaded = (element: any) => {
+  const filterResults: Overloaded = (element: any) => {
     if (property) {
       const propertyValue = getObjectKeys(property);
       return propertyValue.every((value) => getValue(element[value], query));
@@ -41,12 +38,16 @@ export const useAutocomplete = <T>(
     return getValue(element, query);
   };
 
+  const search = () => {
+    const searchResults = data.filter(filterResults);
+    setResults(searchResults);
+  }
+
   const handleSearch = (e: React.SyntheticEvent) => {
     const target = e.target as HTMLInputElement;
     setQuery(target.value);
-    const searchResults = data.filter(findResults);
-    setResults(searchResults);
+    search();
   };
 
-  return { handleSearch, results, query };
+  return { handleSearch, results, query, setQuery, search };
 };
---
+++ src/helpers/testHelpers/testHook.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/ban-types */
+import React from 'react';
+import { Provider } from 'react-redux';
+import { render } from '@testing-library/react';
+import { createStore } from 'redux';
+import { rootReducer } from '../../redux/reducers/rootReducer';
+
+const store = createStore(rootReducer);
+
+type Hook = (...args: any[]) => any;
+
+const TestHook = ({ hook, args = [] }: {hook: Hook, args?: any[]}) => {
+  hook(...args);
+  return null;
+};
+
+export const testHook = (hook: Hook) => render(<Provider store={store}><TestHook hook={hook} /></Provider>);
